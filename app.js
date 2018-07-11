9c4534cfdc83b551b1b1f2b0dac1424818cfb605
colorfulZebra <colorful_zebra@139.com>
2018-07-11 11:41:02 +0800

    check file info restapi

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const fs = require('fs')
 const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
@@ -40,53 +41,86 @@ app.post('/upload/marketlist', upload.single('marketList'), (req, res, next) => 
   })
 })
 
-app.post('/upload/targetlist/xian', upload.single('targetListxa'), (req, res, next) => {
+app.post('/upload/targetlist', upload.single('targetList'), (req, res, next) => {
   res.send({
     filename: req.file.filename
   })
 })
 
-app.post('/upload/targetlist/xianyang', upload.single('targetListxy'), (req, res, next) => {
-  res.send({
-    filename: req.file.filename
-  })
+app.post('/check', (req, res) => {
+  let filepath = `./${config.upload}${req.body.file}`
+  if (fs.existsSync(filepath)) {
+    let fcontent = fs.readFileSync(filepath)
+    let flist = fcontent.toString().trim().split('\n')
+    fcontent = ''
+    let flines = flist.length
+    let ftitle = flist[0]
+    res.send({
+      return: {
+        code: 0,
+        message: 'OK'
+      },
+      data: {
+        lines: flines,
+        title: ftitle
+      }
+    })
+  } else {
+    res.send({
+      return: {
+        code: 10,
+        message: '文件不存在'
+      }
+    })
+  }
 })
 
 app.post('/result/list', (req, res) => {
   req.setTimeout(120*1000)
-  let target = req.body.targetfile
-  let base = req.body.basefile
-  let market = req.body.marketfile
-  if (target === undefined || base === undefined || market === undefined) {
+  if (req.body.targetfile === undefined || req.body.basefile === undefined || req.body.marketfile === undefined) {
     res.send({
       return: {
         code: 10,
-        message: 'targetfile & basefile & marketfile path should not be undefined'
+        message: '全量清单、全量营销清单或目标客户清单文件路径未定义'
       }
     })
-  }
-  let result = {}
-  try {
-    result = getlist(target, base, market)
-  } catch (err) {
+  } else {
+    let target = `./${config.upload}${req.body.targetfile}`
+    let base = `./${config.upload}${req.body.basefile}`
+    let market = `./${config.upload}${req.body.marketfile}`
+    let result = {}
+    try {
+      result = getlist(target, base, market)
+    } catch (err) {
+      res.send({
+        return: {
+          code: 20,
+          message: err
+        }
+      })
+    }
     res.send({
       return: {
-        code: 20,
-        message: err
+        code: 0,
+        message: 'OK'
+      },
+      data: {
+        total: result.total,
+        compliance: result.compliance,
+        non_compliance: result.non_compliance 
       }
     })
   }
-  res.send({
-    return: {
-      code: 0,
-      message: 'OK'
-    },
-    data: {
-      total: result.total,
-      compliance: result.compliance,
-      non_compliance: result.non_compliance 
-    }
-  })
+})
+
+app.get('/download/result', (req, res) => {
+  let filepath = __dirname + '/data/result.txt'
+  res.download(filepath)
+})
+
+app.get('/download/resulterr', (req, res) => {
+  let filepath = __dirname + '/data/result.err.txt'
+  res.download(filepath)
 })
 
 app.listen(config.port, () => {
